1b652270a8f08a686b9331e2cf8a8db868f76327
Manish Kumar <manishkumar.dev08@gmail.com>
2025-04-14 16:16:26 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaInstagram, FaWhatsapp } from "react-icons/fa";
 import "./Footer.css";
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="social-icons">
-        <a href="https://github.com/yourgithub" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/manishk835" target="_blank" rel="noopener noreferrer">
           <FaGithub className="icon github" />
         </a>
         <a href="https://www.linkedin.com/in/yourlinkedin" target="_blank" rel="noopener noreferrer">
@@ -15,11 +15,18 @@ function Footer() {
         <a href="https://www.instagram.com/yourinstagram" target="_blank" rel="noopener noreferrer">
           <FaInstagram className="icon instagram" />
         </a>
+        <a
+          href="https://api.whatsapp.com/send?phone=918302958489&text=Inquiry Hello"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn-whatsapp-bottom-right"
+        >
+          <FaWhatsapp />
+        </a>
+
+
       </div>
       <p className="copyright">© 2025 Manish Kumar | All Rights Reserved</p>
-      <a href="https://api.whatsapp.com/send?phone=8302958489&amp;text=Inquiry Regarding Admission for My Daughter/Son in [Upcoming Session]" class="btn-whatsapp-pulse">
-        <i class="fa fa-phone" aria-hidden="true"></i>  
-    </a>
     </footer>
   );
 }
